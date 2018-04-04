b7251324a01fde195704c0e5e20b36ce2036595c
luemier <luemier@gmail.com>
2018-04-04 23:28:48 +0900

    change *.*

---
+++ ejs.basic2.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+var ejs = require('ejs');
+
+http.createServer(function (request, response) {
+	fs.readFile('ejsPage2.ejs', 'utf8', function (error, data) {
+		response.writeHead(200, {'Content-type': 'text/html' });
+		//response.end(ejs.render(data));
+		response.end(ejs.render(data, {
+			name : 'yunsejin',
+			desc : 'genius !!!'
+		}));
+	});
+}).listen(52273, function () {
+	console.log('Server Running at htt://127.0.0.1:52273');
+});
+
+
